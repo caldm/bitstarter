85ce20c7acb6d9345a9721a99d5df6e2d251f654
Marcos Lara <marcos.lara@gmail.com>
2013-07-10 04:04:01 +0000

    Read index.html and return it's contents

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync('index.html')
+var contents = buf.toString('utf-8')
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
